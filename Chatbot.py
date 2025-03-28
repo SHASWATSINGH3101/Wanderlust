88f7000575a106f349ebd007bacf0ca5829c0a33
shaswat singh <70532135+SHASWATSINGH3101@users.noreply.github.com>
2025-03-28 22:16:46 +0530

    Initial commit: Chatbot.py

---
+++ Chatbot.py
@@ -0,0 +1,5 @@
+from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.
